684e0798e62b22157cef38bb0f20f576e23faf05
Ronald Marcos <48993141+ronmarc97@users.noreply.github.com>
2019-07-01 11:41:06 -0300

    Create functions.h

---
+++ tests/functions.h
@@ -0,0 +1,49 @@
+//
+// Created by ronald on 6/28/2019.
+//
+#include <iostream>
+#include <string>
+#include <map>
+#include <regex>
+#include <math.h>
+
+using namespace std;
+
+double truncate(double& val, unsigned int fractionalDigits)
+{
+    return trunc(val * powf(10, fractionalDigits)) / powf(10, fractionalDigits);
+}
+
+void printData(map <string, int> &data, std::vector<std::string> names) {
+
+    double RDWCV = double(data[names[4]])/(double)100.0;
+    double RDWSD = double(data[names[0]])/(double)100.0;
+
+    cout << "RDWCV=" << truncate(RDWCV, 1) << "," << "RDWSD=" << truncate(RDWSD, 1) << "\\" << "r" << endl;
+    cout << std::endl;
+}
+
+void decodeSequence(map <string, int> &data, std::vector<std::string> names, string input){
+
+    std::string subject(input);
+    std::regex rgx("([0-9]{4})+((\\r)*)");
+    //regex object for matching 4 characters followed
+    // by a Carriage Return: _ _ _ _\r
+
+    try {
+        std::sregex_iterator begin(subject.begin(), subject.end(), rgx);
+        std::sregex_iterator end;
+        string result;
+        int i = 0;
+        // finding all the matching pattern.
+        while (begin != end) {
+            std::smatch match = *begin;
+            data[names[i]] = stoi(match.str()); // store in data.
+            //std::cout << match.str() << "\n";
+            begin++;
+            i++;
+        }
+    } catch (std::regex_error& e) {
+        // Syntax error in the regular expression
+    }
+}
